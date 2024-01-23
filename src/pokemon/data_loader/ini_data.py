81c0f2ba0947b32166effd1b89b85ab8743fd0cd
fcorso2016 <35567462+fcorso2016@users.noreply.github.com>
2024-01-22 21:30:12 -0500

    Changed the structure slightly

--- src/pokemon/data_loader/ini_data.py
+++ src/pokemon/data_loader/ini_data.py
@@ -5,26 +5,26 @@ from typing import Optional, Iterator
 
 class IniData():
     def __init__(self, config_path: str):
-        self._load_config(config_path)
-
-    def _load_config(self, config_path: str):
         with open(config_path, 'r', encoding='utf') as file:
-            self._items: dict[str, dict[str, str]] = {}
-            item_id: Optional[str] = None
-            for line in file:
-                if line.startswith('#'):
-                    continue
+            self.__parse_config(file)
 
-                match = re.match(r'\[(\w+)]', line)
-                if match is not None:
-                    item_id = match.group(1)
-                    self._items[item_id] = {}
+    def __parse_config(self, file):
+        self._items: dict[str, dict[str, str]] = {}
+        item_id: Optional[str] = None
+        for line in file:
+            if line.startswith('#'):
+                continue
 
-                match = re.match(r'(\w+)\s*=\s*(.*)', line)
-                if match is not None and item_id is not None:
-                    key = match.group(1)
-                    value = match.group(2).strip()
-                    self._items[item_id][key] = value
+            match = re.match(r'\[(\w+)]', line)
+            if match is not None:
+                item_id = match.group(1)
+                self._items[item_id] = {}
+
+            match = re.match(r'(\w+)\s*=\s*(.*)', line)
+            if match is not None and item_id is not None:
+                key = match.group(1)
+                value = match.group(2).strip()
+                self._items[item_id][key] = value
 
     def __iter__(self) -> Iterator[tuple[str, dict[str, str]]]:
         return iter(self._items.items())
